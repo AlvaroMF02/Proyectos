d67bb631987c4b490c6c2608e83e21e6e7f6199c
alvaro <alvaromarflo@gmail.com>
2023-10-10 19:16:35 +0200

    matate

--- Unidad1/FECHAS/Ejercicios/fecha1.php
+++ Unidad1/FECHAS/Ejercicios/fecha1.php
@@ -8,13 +8,14 @@
         $fechaValida = checkdate($arrayNumeros1[1],$arrayNumeros1[0],$arrayNumeros1[2]);
         $error_fecha1 = $_POST["fecha1"]=="" || strlen($_POST["fecha1"])!=10 || !$buenosSeparadores1 || !$numerosBuenos1 || !$fechaValida;
 
-        $buenosSeparadores1 = substr($_POST["fecha1"],2,1) =="/" && substr($_POST["fecha1"],5,1) =="/";
-        $arrayNumeros1=explode("/",$_POST["fecha1"]);
-        $numerosBuenos1 = is_numeric($arrayNumeros1[0]) && is_numeric($arrayNumeros1[1]) && is_numeric($arrayNumeros1[2]);
-        $fechaValida = checkdate($arrayNumeros1[1],$arrayNumeros1[0],$arrayNumeros1[2]);
-        $error_fecha1 = $_POST["fecha1"]=="" || strlen($_POST["fecha1"])!=10 || !$buenosSeparadores1 || !$numerosBuenos1 || !$fechaValida;
+        $buenosSeparadores2 = substr($_POST["fecha2"],2,1) =="/" && substr($_POST["fecha2"],5,1) =="/";
+        $arrayNumeros2=explode("/",$_POST["fecha2"]);
+        $numerosBuenos2 = is_numeric($arrayNumeros2[0]) && is_numeric($arrayNumeros2[1]) && is_numeric($arrayNumeros2[2]);
+        $fechaValida2 = checkdate($arrayNumeros2[1],$arrayNumeros2[0],$arrayNumeros2[2]);
+        $error_fecha2 = $_POST["fecha2"]=="" || strlen($_POST["fecha2"])!=10 || !$buenosSeparadores2 || !$numerosBuenos2 || !$fechaValida2;
 
 
+        $errorFormu = $error_fecha1 || $error_fecha2;
     }
 ?>
 
@@ -61,7 +62,7 @@
         <label for="f1">Introduzca una fecha (DD/MM/YYYY)</label>
         <input type="text" id="f1" name="fecha1"  value="<?php if(isset($_POST["fecha1"])) echo $_POST["fecha1"] ?>">   
         
-        <?php /*
+        <?php 
             if (isset($_POST["enviar"]) && $errorFormu) {
 
                 if ($erroFecha1) {
@@ -70,7 +71,7 @@
                 if ($errorTamFech1) {
                     echo "* Fecha mal escrita *";
                 }
-        }*/
+        }
         ?>
         <br>
 
@@ -99,14 +100,15 @@
     <?php
     if (isset($_POST["enviar"]) && !$errorFormu) {
 
-
+        $resul = 0;
+        $resul = mktime(0,0,0,is_numeric($arrayNumeros1[1]) && is_numeric($arrayNumeros1[0]) && is_numeric($arrayNumeros1[2]));
 
 
         echo "</br>";
         echo "</br>";
         echo "<div class='verdoso'>";
         echo "<h2>Fechas - Respuesta</h2>";
-        echo "La palabra ";
+        echo "La diferencia en días entre las dos fechas introducidas es de " .$resul;
         echo "</div>";
     }
 
