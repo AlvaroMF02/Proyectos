b689e69d2f4e4d4415bed296764f4c120194aa4c
AlvaroMF02 <alvaromarflo@gmail.com>
2023-10-11 14:17:06 +0200

    ME MATO

--- Unidad1/FECHAS/Ejercicios/fecha1.php
+++ Unidad1/FECHAS/Ejercicios/fecha1.php
@@ -1,32 +1,24 @@
 <?php
 
+function buenos_separadores($texto)
+{
+    return substr($texto, 2, 1) == "/" && substr($texto, 5, 1) == "/";
+}
+
+function numeros_buenos($texto)
+{
+    return is_numeric(substr($texto, 0, 2)) && is_numeric(substr($texto, 3, 2)) && is_numeric(substr($texto, 6, 4));
+}
+
+function fecha_valida($texto)
+{
+    return checkdate(substr($texto, 3, 2), substr($texto, 0, 2), substr($texto, 6, 4));
+}
 if (isset($_POST["enviar"])) {
 
-    // TIENE /
-    $buenosSeparadores1 = substr($_POST["fecha1"], 2, 1) == "/" && substr($_POST["fecha1"], 5, 1) == "/";
-    if ($_POST["fecha1"] != "") {
-        // DIVIDIR LA FECHA
-        $arrayNumeros1 = explode("/", $_POST["fecha1"]);
-        // SON NUMEROS
-        $numerosBuenos1 = is_numeric($arrayNumeros1[0]) && is_numeric($arrayNumeros1[1]) && is_numeric($arrayNumeros1[2]);
-        // COMPROBAR QUE LA FECHA SEA CORRECTA
-        $fechaValida = checkdate($arrayNumeros1[1], $arrayNumeros1[0], $arrayNumeros1[2]);
-    }
-
     // COMPROBACION DE TODOS LOS ERRORES
-    $error_fecha1 = $_POST["fecha1"] == "" || strlen($_POST["fecha1"]) != 10 || !$buenosSeparadores1 || !$numerosBuenos1 || !$fechaValida;
-
-    // IGUAL CON FECHA 2
-    $buenosSeparadores2 = substr($_POST["fecha2"], 2, 1) == "/" && substr($_POST["fecha2"], 5, 1) == "/";
-    if ($_POST["fecha2"] != "") {
-        $arrayNumeros2 = explode("/", $_POST["fecha2"]);
-        $numerosBuenos2 = is_numeric($arrayNumeros2[0]) && is_numeric($arrayNumeros2[1]) && is_numeric($arrayNumeros2[2]);
-        $fechaValida2 = checkdate($arrayNumeros2[1], $arrayNumeros2[0], $arrayNumeros2[2]);
-    }
-
-
-    $error_fecha2 = $_POST["fecha2"] == "" || strlen($_POST["fecha2"]) != 10 || !$buenosSeparadores2 || !$numerosBuenos2 || !$fechaValida2;
-
+    $error_fecha1 = $_POST["fecha1"] == "" || strlen($_POST["fecha1"]) != 10 || !buenos_separadores($_POST["fecha1"]) || !numeros_buenos($_POST["fecha1"]) || !fecha_valida($_POST["fecha1"]);
+    $error_fecha2 = $_POST["fecha2"] == "" || strlen($_POST["fecha2"]) != 10 || !buenos_separadores($_POST["fecha2"]) || !numeros_buenos($_POST["fecha2"]) || !fecha_valida($_POST["fecha2"]);
 
     $errorFormu = $error_fecha1 || $error_fecha2;
 }
@@ -66,6 +58,9 @@ if (isset($_POST["enviar"])) {
         .titulo {
             text-align: center;
         }
+        .error{
+            color: red;
+        }
     </style>
 </head>
 
@@ -79,20 +74,12 @@ if (isset($_POST["enviar"])) {
                 <input type="text" id="f1" name="fecha1" value="<?php if (isset($_POST["fecha1"])) echo $_POST["fecha1"] ?>">
 
                 <?php
-                if (isset($_POST["enviar"]) && $errorFormu) {
+                if (isset($_POST["enviar"]) && $error_fecha1) {
 
                     if ($_POST["fecha1"] == "") {
-                        echo "* Campo vacio *";
+                        echo "<span class=error>* Campo vacio *</span>";
                     } else {
-                        if ($buenosSeparadores1) {
-                            echo "* La fecha está mal escrita *";
-                        }
-                        if ($numerosBuenos1) {
-                            echo "* No se han escrito números *";
-                        }
-                        if ($fechaValida) {
-                            echo "* La fecha no es valida *";
-                        }
+                        echo "<span class=error>* Fecha inválida *</span>";
                     }
                 }
                 ?>
@@ -102,16 +89,12 @@ if (isset($_POST["enviar"])) {
                 <label for="f2">Introduzca una fecha (DD/MM/YYYY)</label>
                 <input type="text" id="f2" name="fecha2" value="<?php if (isset($_POST["fecha2"])) echo $_POST["fecha2"] ?>">
                 <?php
-                if (isset($_POST["enviar"]) && $errorFormu) {
+                if (isset($_POST["enviar"]) && $error_fecha2) {
 
-                    if ($_POST["enviar"] == "") {
-                        echo "* Campo vacio *";
-                    } elseif ($buenosSeparadores2) {
-                        echo "* La fecha está mal escrita *";
-                    } elseif ($numerosBuenos2) {
-                        echo "* No se han escrito números *";
-                    } elseif ($fechaValida2) {
-                        echo "* La fecha no es valida *";
+                    if ($_POST["fecha2"] == "") {
+                        echo "<span class=error>* Campo vacio *</span>";
+                    } else {
+                        echo "<span class=error>* Fecha inválida *</span>";
                     }
                 }
 
@@ -127,15 +110,21 @@ if (isset($_POST["enviar"])) {
     <?php
     if (isset($_POST["enviar"]) && !$errorFormu) {
 
-        $resul = 0;
-        $resul = mktime(0, 0, 0, is_numeric($arrayNumeros1[1]) && is_numeric($arrayNumeros1[0]) && is_numeric($arrayNumeros1[2]));
+        $fecha1=explode("/",$_POST["fecha1"]);
+        $fecha2=explode("/",$_POST["fecha2"]);
 
+        $tiempo1 = mktime(0,0,0,$fecha1[1],$fecha1[0],$fecha1[2]);
+        $tiempo2 = mktime(0,0,0,$fecha2[1],$fecha2[0],$fecha2[2]);
+
+        $segundosDiff = abs($tiempo1 - $tiempo2);
+
+        $diasPasados = floor($segundosDiff/(60*60*24));
 
         echo "</br>";
         echo "</br>";
         echo "<div class='verdoso'>";
         echo "<h2>Fechas - Respuesta</h2>";
-        echo "La diferencia en días entre las dos fechas introducidas es de " . $resul;
+        echo "La diferencia en días entre las dos fechas introducidas es de " . $diasPasados;
         echo "</div>";
     }
 
---
+++ Unidad1/FECHAS/Ejercicios/fecha2.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Ejercicio 2</title>
+    <style>
+        form {
+            background-color: lightblue;
+            margin-left: 20%;
+            margin-right: 20%;
+            padding: 1%;
+            border-color: black;
+            border: 3px solid black;
+        }
+
+        .verdoso {
+            background-color: lightgreen;
+            margin-left: 20%;
+            margin-right: 20%;
+            padding: 1%;
+            border-color: black;
+            border: 3px solid black;
+        }
+
+        h2 {
+            text-align: center
+        }
+
+        .titulo {
+            text-align: center;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="principal">
+
+        <form action="fecha2.php" method="post">
+            <h1 class="titulo">Fechas-Formulario</h1>
+            <p>
+            <p>Introduzca una fecha:</p>
+            Día:
+            <select name="dia1" id="dia1">
+                <?php
+                for ($i = 1; $i <= 31; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+            Mes:
+            <select name="mes1" id="mes1">
+                <?php
+                $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+                foreach ($meses as  $mes) {
+                    echo "<option>" . $mes . "</option>";
+                }
+                ?>
+            </select>
+            Año:
+            <select name="anyo1" id="anyo1">
+                <?php
+                for ($i = 1950; $i <= 2023; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+
+
+            <p>Introduzca una fecha:</p>
+            Día:
+            <select name="dia2" id="dia2">
+                <?php
+                for ($i = 1; $i <= 31; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+            Mes:
+            <select name="mes2" id="mes2">
+                <?php
+                $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+
+                foreach ($meses as  $mes) {
+                    echo "<option>" . $mes . "</option>";
+                }
+                ?>
+            </select>
+            Año:
+            <select name="anyo2" id="anyo2">
+                <?php
+                for ($i = 1950; $i <= 2023; $i++) {
+                    echo "<option>" . $i . "</option>";
+                }
+                ?>
+            </select>
+            </p>
+            <button type="submit" name="enviar">Calcular</button>
+
+        </form>
+    </div>
+
+
+    <?php
+    if (isset($_POST["enviar"])) {
+        echo "</br>";
+        echo "</br>";
+        echo "<div class='verdoso'>";
+        echo "<h2>Fechas - Respuesta</h2>";
+        echo "La diferencia en días entre las dos fechas introducidas es de " ;
+        echo "</div>";
+    }
+
+    ?>
+</body>
+
+</html>
