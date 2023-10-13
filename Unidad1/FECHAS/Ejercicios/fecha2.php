603b3925382df95180c7b419d12aec808d23a196
alvaro <alvaromarflo@gmail.com>
2023-10-13 18:17:00 +0200

    2 terminado

--- Unidad1/FECHAS/Ejercicios/fecha2.php
+++ Unidad1/FECHAS/Ejercicios/fecha2.php
@@ -84,8 +84,6 @@
             Mes:
             <select name="mes2" id="mes2">
                 <?php
-                $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-
                 foreach ($meses as  $mes) {
                     echo "<option>" . $mes . "</option>";
                 }
@@ -110,11 +108,32 @@
     if (isset($_POST["enviar"])) {
 
 
+        
+        // SACO LOS SEGUNDOS DE CADA DIA
+        // EN BUCLE PARA CONSEGUIR LOS NUMEROS DE LOS MESES
+        // FALTA SUMAR UNO A LAS KEY(FUNCIONA NS PQ)
+        foreach ($meses as $key => $value) {
+            if ($value == $_POST["mes1"]) {
+                $tiempo1 = mktime(0,0,0, $key,$_POST["dia1"],$_POST["anyo1"]);
+                //echo "Mes:".$key." dia: ". $_POST["dia1"]. "año: ".$_POST["anyo1"];
+            }
+            if ($value == $_POST["mes2"]) {
+                $tiempo2 = mktime(0,0,0, $key,$_POST["dia2"],$_POST["anyo2"]);
+                //echo "Mes:".$key." dia: ". $_POST["dia2"]. "año: ".$_POST["anyo2"];
+            }
+        }
+
+        // LOS RESTO Y LOS PONGO POSITIVOS POR SI TIEMPO1 ES MENOR
+        $segundosDiff = abs($tiempo1 - $tiempo2);
+
+        // CALCULO LOS DIAS BASNDOME EN LOS SEGUNDOS
+        $diasPasados = floor($segundosDiff/(60*60*24));
+
         echo "</br>";
         echo "</br>";
         echo "<div class='verdoso'>";
         echo "<h2>Fechas - Respuesta</h2>";
-        echo "La diferencia en días entre las dos fechas introducidas es de ".$_POST["dia1"].$_POST["mes1"].$_POST["anyo1"] ;
+        echo "La diferencia en días entre las dos fechas introducidas es de ".$diasPasados;
         echo "</div>";
     }
 
