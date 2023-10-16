11d08928633a3f076559090497d0df61853197bb
AlvaroMF02 <alvaromarflo@gmail.com>
2023-10-16 09:12:46 +0200

    FECHA 2 HECHA

--- Unidad1/FECHAS/Ejercicios/fecha2.php
+++ Unidad1/FECHAS/Ejercicios/fecha2.php
@@ -1,5 +1,18 @@
+<?php
+
+if (isset($_POST["enviar"])) {
+
+    $error_fecha1 = !checkdate($_POST["mes1"], $_POST["dia1"], $_POST["anyo1"]);
+    $error_fecha2 = !checkdate($_POST["mes2"], $_POST["dia2"], $_POST["anyo2"]);
+
+    $errorFormu = $error_fecha1 || $error_fecha2;
+}
+?>
+
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
@@ -44,59 +57,102 @@
             <h1 class="titulo">Fechas-Formulario</h1>
             <p>
             <p>Introduzca una fecha:</p>
-            Día:
+            <label for="dia1">Día:</label>
             <select name="dia1" id="dia1">
                 <?php
                 for ($i = 1; $i <= 31; $i++) {
-                    echo "<option>" . $i . "</option>";
+                    if (isset($_POST["enviar"]) && $_POST["dia1"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . sprintf("%02d", $i) . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . sprintf("%02d", $i) . "</option>";
+                    }
                 }
                 ?>
             </select>
-            Mes:
+
+            <label for="mes1">Mes:</label>
             <select name="mes1" id="mes1">
                 <?php
-                $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+                $meses = array(1 => "Enero", 2 => "Febrero", 3 => "Marzo", 4 => "Abril", 5 => "Mayo", 6 => "Junio", 7 => "Julio", 8 => "Agosto", 9 => "Septiembre", 10 => "Octubre", 11 => "Noviembre", 12 => "Diciembre");
 
-                foreach ($meses as  $mes) {
-                    echo "<option>" . $mes . "</option>";
+                for ($i = 1; $i <= 12; $i++) {
+                    if (isset($_POST["enviar"]) && $_POST["mes1"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . $meses[$i] . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . $meses[$i] . "</option>";
+                    }
                 }
                 ?>
             </select>
-            Año:
+
+            <label for="anyo1">Año:</label>
             <select name="anyo1" id="anyo1">
                 <?php
-                for ($i = 1970; $i <= 2023; $i++) {
-                    echo "<option>" . $i . "</option>";
+                $anyoActual = date("Y");
+                for ($i = $anyoActual - 50; $i <= $anyoActual; $i++) {
+                    if (isset($_POST["enviar"]) && $_POST["anyo1"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . $i . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . $i . "</option>";
+                    }
                 }
                 ?>
-            </select>
+                
+                </select>
+
+                <?php
+                if (isset($_POST["enviar"]) && $error_fecha2) {
+                    echo "<span class='error'>* Fecha no válida *</span>";
+                }
+                ?>
 
 
             <p>Introduzca una fecha:</p>
-            Día:
+            <label for="dia2">Día:</label>
             <select name="dia2" id="dia2">
                 <?php
                 for ($i = 1; $i <= 31; $i++) {
-                    echo "<option>" . $i . "</option>";
+                    if (isset($_POST["enviar"]) && $_POST["dia2"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . sprintf("%02d", $i) . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . sprintf("%02d", $i) . "</option>";
+                    }
                 }
                 ?>
             </select>
-            Mes:
+            <label for="mes2">Mes:</label>
             <select name="mes2" id="mes2">
                 <?php
-                foreach ($meses as  $mes) {
-                    echo "<option>" . $mes . "</option>";
+                for ($i = 1; $i <= 12; $i++) {
+                    if (isset($_POST["enviar"]) && $_POST["mes2"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . $meses[$i] . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . $meses[$i] . "</option>";
+                    }
                 }
                 ?>
             </select>
-            Año:
+            <label for="anyo2">Año:</label>
             <select name="anyo2" id="anyo2">
                 <?php
-                for ($i = 1970; $i <= 2023; $i++) {
-                    echo "<option>" . $i . "</option>";
+                for ($i = $anyoActual - 50; $i <= $anyoActual; $i++) {
+                    if (isset($_POST["enviar"]) && $_POST["anyo2"] == $i) {
+                        echo "<option selected value='" . $i . "'>" . $i . "</option>";
+                    } else {
+                        echo "<option value='" . $i . "'>" . $i . "</option>";
+                    }
                 }
                 ?>
-            </select>
+                
+                </select>
+
+                <?php
+                if (isset($_POST["enviar"]) && $error_fecha2) {
+                    echo "<span class='error'>* Fecha no válida *</span>";
+                }
+                ?>
+
+            
             </p>
             <button type="submit" name="enviar">Calcular</button>
 
@@ -105,35 +161,23 @@
 
 
     <?php
-    if (isset($_POST["enviar"])) {
+    if (isset($_POST["enviar"])&&!$errorFormu) {
 
 
-        
-        // SACO LOS SEGUNDOS DE CADA DIA                                                LAS FECHAS SE BORRAN
-        // EN BUCLE PARA CONSEGUIR LOS NUMEROS DE LOS MESES
-        // FALTA SUMAR UNO A LAS KEY(FUNCIONA NS PQ)
-        foreach ($meses as $key => $value) {
-            if ($value == $_POST["mes1"]) {
-                $tiempo1 = mktime(0,0,0, $key,$_POST["dia1"],$_POST["anyo1"]);
-                //echo "Mes:".$key." dia: ". $_POST["dia1"]. "año: ".$_POST["anyo1"];
-            }
-            if ($value == $_POST["mes2"]) {
-                $tiempo2 = mktime(0,0,0, $key,$_POST["dia2"],$_POST["anyo2"]);
-                //echo "Mes:".$key." dia: ". $_POST["dia2"]. "año: ".$_POST["anyo2"];
-            }
-        }
+        $tiempo1 = strtotime($_POST["anyo1"]."/".$_POST["mes1"]."/".$_POST["dia1"]);
+        $tiempo2 = strtotime($_POST["anyo2"]."/".$_POST["mes2"]."/".$_POST["dia2"]);
 
         // LOS RESTO Y LOS PONGO POSITIVOS POR SI TIEMPO1 ES MENOR
         $segundosDiff = abs($tiempo1 - $tiempo2);
 
         // CALCULO LOS DIAS BASNDOME EN LOS SEGUNDOS
-        $diasPasados = floor($segundosDiff/(60*60*24));
+        $diasPasados = floor($segundosDiff / (60 * 60 * 24));
 
         echo "</br>";
         echo "</br>";
         echo "<div class='verdoso'>";
         echo "<h2>Fechas - Respuesta</h2>";
-        echo "La diferencia en días entre las dos fechas introducidas es de ".$diasPasados;
+        echo "La diferencia en días entre las dos fechas introducidas es de " . $diasPasados;
         echo "</div>";
     }
 
