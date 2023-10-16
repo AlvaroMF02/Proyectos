e5a4c3251a26edf794cd3bb021167fb03682df99
AlvaroMF02 <alvaromarflo@gmail.com>
2023-10-16 09:23:09 +0200

    hecho

--- Unidad1/FECHAS/Ejercicios/fecha3.php
+++ Unidad1/FECHAS/Ejercicios/fecha3.php
@@ -17,7 +17,7 @@ if (isset($_POST["enviar"])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Ejercicio 1</title>
+    <title>Ejercicio 3</title>
     <style>
         form {
             background-color: lightblue;
@@ -56,26 +56,26 @@ if (isset($_POST["enviar"])) {
         <form action="fecha3.php" method="post">
             <h1 class="titulo">Fechas-Formulario</h1>
             <p>
-                <label for="f1">Introduzca una fecha (DD/MM/YYYY)</label>
-                <input type="date" name="fecha1" step="1">
+                <label for="f1">Introduzca una fecha:</label>
+                <input type="date" name="fecha1" step="1" value="<?php if(isset($_POST["fecha1"])) echo $_POST["fecha1"]?>">
 
                 <?php
                 if (isset($_POST["enviar"]) && $error_fecha1) {
 
                     if ($_POST["fecha1"] == "") 
-                        echo "<span class=error>* Campo vacio *</span>";
+                        echo "<span class=error>* No has seleccionado una fecha *</span>";
                 }
                 ?>
                 <br>
 
 
-                <label for="f2">Introduzca una fecha (DD/MM/YYYY)</label>
-                <input type="date" name="fecha2" step="1">
+                <label for="f2">Introduzca una fecha: </label>
+                <input type="date" name="fecha2" step="1" value="<?php if(isset($_POST["fecha2"])) echo $_POST["fecha2"]?>">
                 <?php
                 if (isset($_POST["enviar"]) && $error_fecha2) {
 
                     if ($_POST["fecha2"] == "")
-                        echo "<span class=error>* Campo vacio *</span>";
+                        echo "<span class=error>* No has seleccionado una fecha *</span>";
                     
                 }
 
@@ -92,12 +92,20 @@ if (isset($_POST["enviar"])) {
     if (isset($_POST["enviar"]) && !$errorFormu) {
 
         // LA FECHA SE BORRA Y NS SACARLAS
+        $tiempo1 = strtotime($_POST["fecha1"]);
+        $tiempo2 = strtotime($_POST["fecha2"]);
+
+        // LOS RESTO Y LOS PONGO POSITIVOS POR SI TIEMPO1 ES MENOR
+        $segundosDiff = abs($tiempo1 - $tiempo2);
+
+        // CALCULO LOS DIAS BASNDOME EN LOS SEGUNDOS
+        $diasPasados = floor($segundosDiff / (60 * 60 * 24));
 
         echo "</br>";
         echo "</br>";
         echo "<div class='verdoso'>";
         echo "<h2>Fechas - Respuesta</h2>";
-        echo "La diferencia en días entre las dos fechas introducidas es de " ;
+        echo "La diferencia en días entre las dos fechas introducidas es de " . $diasPasados;
         echo "</div>";
     }
 
