19beba4e44d8283653b61ca70a8fd604c0262f9f
AlvaroMF02 <alvaromarflo@gmail.com>
2023-10-18 10:11:39 +0200

    miua

--- Unidad1/FICHEROS/ejercicios/ejer4/Ejer4.php
+++ Unidad1/FICHEROS/ejercicios/ejer4/Ejer4.php
@@ -1,9 +1,19 @@
 <?php 
 if (isset($_POST["enviar"])) {
-    // SI NO HA MANDADO NADAS, SI TIENE ALGUN ERROR
-    $errorText = $_FILES["archivo"]["name"] != "" && ($_FILES["archivo"]["error"] || !getimagesize($_FILES["archivo"]["tmp_name"]) || $_FILES["archivo"]["size"] > 500 * 1024);
+
+    // $error_archivo = $_FILES["archivo"]["name"] != "" && ($_FILES["archivo"]["error"] || !getimagesize($_FILES["archivo"]["tmp_name"]) || $_FILES["archivo"]["size"] > 500 * 1024);
 
 
+    // SI TIENE UN ERROR DE ARCHIVO
+    $errorArchivo = $_FILES["archivo"]["error"];
+    
+    // SI EL FORMATO ES CORRECTO?
+    $errorFormato = !getimagesize($_FILES["archivo"]["tmp_name"]);
+
+    // SI SUPERA EL TAMAÑO
+    $errorTaman = $_FILES["archivo"]["size"] > 2500 * 1024;
+    
+    $errorSubidaTxt = $errorArchivo ||  $errorFormato|| $errorTaman;
 }
 ?>
 
@@ -25,10 +35,23 @@ if (isset($_POST["enviar"])) {
     <form action="Ejer4.php" method="post">
         <h1>Ejercicio 4</h1>
         
-        <label for="fich">Suba un archivo de texto</label>
+        <label for="fich">Suba un archivo de texto (Max. 2,5 Mb)</label>
         <input type="file" accept="txt*/" name="archivo" id="fich">
+        <?php 
+            if (isset($_POST["enviar"]) && $errorSubidaTxt) {
+                if ($errorArchivo) {
+                    echo "<span class=error>No se ha podido subir el archivo</span>";
+                }
+                if ($errorFormato) {
+                    echo "<span class=error>El formato debe ser .txt</span>";
+                }
+                if ($errorTaman) {
+                    echo "<span class=error>El archivo es demasiado grande</span>";
+                }
+            }
+        ?>
 
-        <button type="submit" name="enviar"></button>
+        <br><button type="submit" name="enviar">Contar letras</button>
 
     </form>
 
