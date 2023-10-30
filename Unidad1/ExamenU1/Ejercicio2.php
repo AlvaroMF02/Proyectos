47a2e3d45ad01c120818f7c516bb84c4d3881ba4
AlvaroMF02 <alvaromarflo@gmail.com>
2023-10-30 09:35:15 +0100

    2 sin hacer

--- Unidad1/ExamenU1/Ejercicio2.php
+++ Unidad1/ExamenU1/Ejercicio2.php
@@ -2,6 +2,71 @@
 if (isset($_POST["enviar"])) {
     $errorTexto = $_POST["texto"] == "";
 }
+function mi_explode($sep,$texto){
+    $respuesta = [];
+    $l_texto = mi_strlen($texto);
+    $i = 0;
+
+    // QUITAR SEPARADORES DEL PRINCIPIO ,,,,
+    while ($i<$l_texto && $texto[$i]==$sep) {
+        $i++;
+    }
+    // YA QUITADOS LOS DE ALANTE:
+    if($i<$l_texto){
+        $j = 0;
+        // METE LA PRIMERA LETRA
+        $respuesta[$j] = $texto[$i];
+        // HASTA EL FINAL
+        for ($i=$i+1; $i<$l_texto ; $i++) { 
+            // SI NO ES SEPARADOR LO METE EN AUX
+            if($texto[$i]!=$sep){
+                $respuesta[$j] .=$texto[$i];
+            }else{
+                // HE ENCONTRADO UN SEPARADPOR
+                // LOS QUITO OTRA VEZ
+                while ($i<$l_texto && $texto[$i]==$sep) {
+                    $i++;
+                }
+                // PARA LOS QUE SE REPITEN AL FINAL
+                if($i<$l_texto){
+                    $j++;
+                    $respuesta[$j]=$texto[$i];
+                }
+            }
+        }
+    }
+    return $respuesta;
+}
+function tiene_vocales($texto){
+    $vocales["a"]=1;
+    $vocales["e"]=1;
+    $vocales["i"]=1;
+    $vocales["o"]=1;
+    $vocales["u"]=1;
+    $vocales["A"]=1;
+    $vocales["E"]=1;
+    $vocales["I"]=1;
+    $vocales["O"]=1;
+    $vocales["U"]=1;
+
+    for ($i=0; $i < strlen($texto); $i++) { 
+        if(isset($vocales[$texto[$i]])){
+            $tiene =true;
+        }
+    }
+
+    return $tiene;
+}
+function filtrar_sin_vocales($arr_palabra) {
+    $respuesta=[];
+    for ($i=0; $i < count($arr_palabra) ; $i++) { 
+        // FALTA TODO :)
+        if(!tiene_vocales($arr_palabra[$i])){
+            $respuesta[]=$arr_palabra[$i];
+        }
+    }
+    return $respuesta;
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -52,5 +117,4 @@ if (isset($_POST["enviar"])) {
     ?>
 
 </body>
-
 </html>
