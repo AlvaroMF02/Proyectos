37bdcf43694738fd0796a30f59e1f9a43cfcc50c
AlvaroMF02 <alvaromarflo@gmail.com>
2023-11-08 13:41:04 +0100

    borrado hecho

--- Unidad2/PRIMER_CRUD/index.php
+++ Unidad2/PRIMER_CRUD/index.php
@@ -1,3 +1,33 @@
+<?php
+// funciones
+require "src/constantes_funciones.php";
+
+// Borrar un usuario
+// Si le he dado al boton de continuar al borrar
+if(isset($_POST["btnContBorrar"])){
+    try {
+        $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro");
+        mysqli_set_charset($conexion, "utf8");
+    } catch (Exception $e) {
+        // creo una página mostrando el error
+        die(error_page("Practica 1 CRUD", "<h1>Primer CRUD</h1><p>No se ha podido conectar a la base de datos:" . $e->getMessage() . "</p>"));
+    }
+
+    // Borrar al usuario
+    try {
+        $consulta = "delete from usuarios where id_usuario='" . $_POST["btnContBorrar"] . "'";
+        $resultado = mysqli_query($conexion, $consulta);
+    } catch (Exception $e) {
+        mysqli_close($conexion);
+        die(error_page("Practica 1 CRUD", "<h1>Primer CRUD</h1><p>No se ha podido ejecutar la consulta:" . $e->getMessage() . "</p>"));
+    }
+    mysqli_close($conexion);
+    header("Location:index.php");
+    exit();
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -63,7 +93,7 @@
     while ($tupla = mysqli_fetch_assoc($resultado)) {
         echo "<tr>";
         echo "<td><form action='index.php' method='post'><button class ='enlace' title='Detalles del usuario' value='" . $tupla["id_usuario"] . "' type='submit' name='btnDetalle'>" . $tupla["nombre"] . "</button></form></td>";
-        echo "<td><form action='index.php' method='post'><button class ='enlace' title='Borrar a un usuaro' value='" . $tupla["id_usuario"] . "' type='submit' name='btnBorrar'><img src='images/borrar.png' title='Borrar usuario' alt='Borrar'></button></form></td>";
+        echo "<td><form action='index.php' method='post'><input type='hidden' name='nombre_usuario' value='" . $tupla["nombre"] . "'><button class ='enlace' title='Borrar a un usuaro' value='" . $tupla["id_usuario"] . "' type='submit' name='btnBorrar'><img src='images/borrar.png' title='Borrar usuario' alt='Borrar'></button></form></td>";
         echo "<td><form action='index.php' method='post'><button class ='enlace' title='Editar a un usuaro' value='" . $tupla["id_usuario"] . "' type='submit' name='btnEditar'><img src='images/editar.png' title='Editar usuario' alt='Borrar'></button></form></td>";
         echo "</tr>";
     }
@@ -91,6 +121,9 @@
             echo "<strong>Usuario: </strong>" . $datos_usuario["usuario"] . "<br>";
             echo "<strong>Email: </strong>" . $datos_usuario["email"] . "<br>";
             echo "</p>";
+
+            // Mirar en la función de arriba
+
         } else {
             echo "<p>El usuario seleccionado ya no se encuentra en la base de datos</p>";
         }
@@ -102,7 +135,17 @@
         echo "<button type='submit'>Volver</button>";
 
     } elseif (isset($_POST["btnBorrar"])) {
-        echo "borra";
+        // Estas seguro de que quieres borrar a x?
+        echo "<p>Se dispone usted a borrar al usuario <strong>".$_POST["nombre_usuario"]."</strong></p>";
+        // Form para continuar o cancelar
+        echo "<form action='index.php' method='post'>";
+        // Continuar coge el valor del boton para conseguir el id
+        echo "<p><button type='submit' name='btnContBorrar' value='".$_POST["btnBorrar"]."'>Continuar</button>";
+        echo "<button type='submit'>Atrás</button></p>";
+        echo "</form>";
+
+
+
 
     } elseif (isset($_POST["btnEditar"])) {
         echo "edita";
---
+++ Unidad2/PRIMER_CRUD/src/constantes_funciones.php
@@ -0,0 +1,33 @@
+<?php
+// Muestra una página nueva si hay un error en la conexion
+function error_page($title, $body)
+{
+    $page = '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>' . $title . '</title>
+            </head>
+            <body>' . $body . '</body>
+            </html>';
+    return $page;
+}
+
+// Algo está mal pq no me dice los repetidos
+function repetido($conexion, $tabla, $columna, $valor)
+{
+    // comprobar que la consulta está bien
+    try {
+        $consulta = "select * from " . $tabla . " where " . $columna . "='" . $valor . "'";
+        $resultado = mysqli_query($conexion, $consulta);
+        $respuesta = mysqli_num_rows($resultado) > 0;
+        mysqli_free_result($resultado);
+    } catch (Exception $e) {
+        mysqli_close($conexion);
+        $respuesta(error_page("Practica 1 CRUD", "<h1>Primer CRUD</h1><p>No se ha podido hacer la consulta:" . $e->getMessage() . "</p>"));
+    }
+
+    return $respuesta;
+}
+?>
