36beb91455437df62c8ed9a71015b83775755806
AlvaroMF02 <alvaromarflo@gmail.com>
2023-11-22 14:33:09 +0100

    mostrar la tabla

--- Unidad2/practica9/index.php
+++ Unidad2/practica9/index.php
@@ -3,6 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table,td,th{border:1px solid black;}
+        table{border-collapse:collapse;text-align:center;width:90%;margin:0 auto}
+        th{background-color:#CCC}
+        table img{width:50px;}
+        .enlace{border:none;background:none;cursor:pointer;color:blue;text-decoration:underline}
+        .error{color:red}
+        .foto_detalle{height:250px}
+        .paralelo{display:flex}
+        .centrado{text-align:center}
+    </style>
     <title>Practica 9</title>
 </head>
 <body>
