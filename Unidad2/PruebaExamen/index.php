5b55ff6ecf1e44bd55f84b0d58010355a3253cc2
AlvaroMF02 <alvaromarflo@gmail.com>
2023-11-28 10:16:06 +0100

    cositas que voy a copiar :)

--- Unidad2/PruebaExamen/index.php
+++ Unidad2/PruebaExamen/index.php
@@ -1,10 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <style>
+        table {
+            text-align: center;
+        }
+
+        table,
+        tr,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th {
+            background-color: #CCC;
+        }
+
+        img {
+            height: 100px;
+        }
+
+        .enlace {
+            background: none;
+            color: blue;
+            text-decoration: underline;
+            border: none;
+            cursor: pointer;
+        }
+
+        .negrita {
+            font-weight: bold;
+        }
+
+        .error {
+            color: red;
+        }
+    </style>
 </head>
+
 <body>
     <h1>Notas de los alumnos</h1>
     <?php
@@ -12,9 +51,12 @@
 
     if (isset($_POST["btnNotas"])) {
         require "vistas/vistaNotas.php";
+    }else if (isset($_POST["btnBorrarNota"])){
+        require "vistas/vistaBorrar.php";
     }
 
     ?>
 
 </body>
+
 </html>
