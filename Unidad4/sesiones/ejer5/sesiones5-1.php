e5f594b8863590fa02e182b22144f3d87e7c39b0
alvaro <alvaromarflo@gmail.com>
2023-12-09 22:35:09 +0100

    diseño mal hecho

--- Unidad4/sesiones/ejer5/sesiones5-1.php
+++ Unidad4/sesiones/ejer5/sesiones5-1.php
@@ -20,6 +20,35 @@ if (!isset($_SESSION["bola"])) {
         .textCentrado {
             text-align: center;
         }
+
+        svg {
+            border: 3px solid black;
+        }
+
+        button {
+            width: 3rem;
+            height: 3rem;
+        }
+
+        button#volver {
+            width: 5rem;
+            height: 3rem;
+        }
+
+        div {
+            width: 12rem;
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+        }
+
+        button#izquierda {
+            align-self: flex-start;
+        }
+
+        button#derecha {
+            align-self: flex-end;
+        }
     </style>
 </head>
 
@@ -28,14 +57,20 @@ if (!isset($_SESSION["bola"])) {
     <p>Haga click en los botones para mover el punto:</p>
 
     <form action="sesiones5-2.php">
-        <button type="submit" name="boton" value="izq">⬅️</button><button type="submit" name="boton" value="der">➡️</button> <br>
 
-        <svg version="1.1" xmlns=http://www.w3.org/2000/svg width="600px" height="20px" viewbox="-300 0 600 20">
-            <circle cx="<?php echo $_SESSION['bola']?>" cy="10" r="8" fill="red" /> 
+        <div>
+            <button id="arriba" type="submit" name="boton" value="arriba"> &#x1F446;</button>
+            <button id="derecha" type="submit" name="boton" value="derecha"> &#x1F449;</button>
+            <button id="volver" type="submit" name="boton" value="volver">Volver al centro</button>
+            <button id="izquierda" type="submit" name="boton" value="izquierda"> &#x1F448;</button>
+            <button id="abajo" type="submit" name="boton" value="abajo"> &#x1F447;</button>
+
+        </div>
+        <svg version="1.1" xmlns=http://www.w3.org/2000/svg width="600px" height="600px" viewbox="-300 0 600 20">
+            <circle cx="<?php echo $_SESSION['bola'] ?>" cy="10" r="8" fill="red" />
         </svg>
-        <br>
-        <button type="submit" name="boton" value="volver">Volver al centro</button>
-        <?php echo $_SESSION['bola']?>
+
+
     </form>
 
 </body>
