a73e9afa7f9e698c6c4cc46884c0bb09e670b315
AlvaroMF02 <alvaromarflo@gmail.com>
2024-01-15 09:17:46 +0100

    mal

---
+++ Unidad5/Paginacion/Primer_CRUD/src/ctes_funciones.php
@@ -0,0 +1,40 @@
+<?php
+//CTES base de datos
+
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "jose");
+define("CLAVE_BD", "josefa");
+define("NOMBRE_BD", "bd_foro");
+
+
+function error_page($title, $body)
+{
+    $page = '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>' . $title . '</title>
+    </head>
+    <body>' . $body . '</body>
+    </html>';
+    return $page;
+}
+
+function repetido($conexion, $tabla, $columna, $valor, $columna_clave = null, $valor_clave = null)
+{
+    try {
+        if (isset($columna_clave))
+            $consulta = "select * from " . $tabla . " where " . $columna . "= ? AND " . $columna_clave . "<> ?";
+        else
+            $consulta = "select * from " . $tabla . " where " . $columna . "= ?";
+
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$nombre, $usuario, $clave, $email]);
+        
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        die("<p>No se ha podido hacer la consulta: " . $e->getMessage() . "</p></body></html>");
+    }
+}
