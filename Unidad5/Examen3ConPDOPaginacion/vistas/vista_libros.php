4cd900f6743b115755bae14ae7e46ee83899afbd
AlvaroMF02 <alvaromarflo@gmail.com>
2024-01-17 14:36:54 +0100

    miau

--- Unidad5/Examen3ConPDOPaginacion/vistas/vista_libros.php
+++ Unidad5/Examen3ConPDOPaginacion/vistas/vista_libros.php
@@ -3,30 +3,28 @@
 echo "<h3>Listado de los libros</h3>";
 // consulta con la que cojo todos los libros
 
-try{
-        
+try {
+
     $consulta = "select * from libros";
-    $sentencia=$conexion->prepare($consulta);
+    $sentencia = $conexion->prepare($consulta);
     $sentencia->execute();
-}
-catch(PDOException $e)
-{
-    $sentencia=null;
-    $conexion=null;
-    session_destroy();                      // No entiendo este session destroy
-    die("<p>No he podido realizar la consulta: ".$e->getMessage()."</p></body></html>");
+} catch (PDOException $e) {
+    $sentencia = null;
+    $conexion = null;
+    session_destroy();
+    die("<p>No he podido realizar la consulta: " . $e->getMessage() . "</p></body></html>");
 }
 
-$tupla=$sentencia->fetch(PDO::FETCH_ASSOC);
+// coge todo
+$resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
 // muestro los libros con el bucle
-foreach($tupla as $resultado){
+foreach ($resultado as $tupla) {
     echo "<div class='fotos'>";
-    echo "<img src='img/".$tupla["portada"]."' alt='imagen libro' title='imagen libro'><br>";
-    echo $tupla["titulo"]." - ".$tupla["precio"]."€";
+    echo "<img src='img/" . $tupla["portada"] . "' alt='imagen libro' title='imagen libro'><br>";
+    echo $tupla["titulo"] . " - " . $tupla["precio"] . "€";
     echo "</div>";
 }
 
 // libero el resultado
 $conexion = null;
-?>
--- Unidad5/Examen3ConPDOPaginacion/vistas/vista_normal.php
+++ Unidad5/Examen3ConPDOPaginacion/vistas/vista_normal.php
@@ -1,28 +1,50 @@
 <!DOCTYPE html>
-    <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Examen3 Curso 23-24</title>
-        <style>
-            .enlinea{display:inline}
-            .enlace{border:none;background:none;text-decoration:underline;color:blue;cursor:pointer}
-            img{height:200px}
-            div.fotos{text-align:center;width:30%;margin-top:2.5%;margin-left:2.5%;float:left}
-        </style>
-    </head>
-    <body>
-        <h1>Librería (normal)</h1>
-        <div>Bienvenido <strong><?php echo $datos_usuario_logueado["lector"];?></strong> - 
-            <form class='enlinea' action="index.php" method="post">
-                <button class='enlace' type="submit" name="btnSalir">Salir</button>
-            </form>
-        </div>
+<html lang="es">
 
-        <?php
-        // vuelvo a hacer un listado de los libros en la parte del lector normal
-        require "vistas/vista_libros.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen3 Curso 23-24</title>
+    <style>
+        .enlinea {
+            display: inline
+        }
 
-        ?>
-    </body>
-    </html>
+        .enlace {
+            border: none;
+            background: none;
+            text-decoration: underline;
+            color: blue;
+            cursor: pointer
+        }
+
+        img {
+            height: 200px
+        }
+
+        div.fotos {
+            text-align: center;
+            width: 30%;
+            margin-top: 2.5%;
+            margin-left: 2.5%;
+            float: left
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Librería (normal)</h1>
+    <div>Bienvenido <strong><?php echo $datos_usuario_logueado["lector"]; ?></strong> -
+        <form class='enlinea' action="index.php" method="post">
+            <button class='enlace' type="submit" name="btnSalir">Salir</button>
+        </form>
+    </div>
+
+    <?php
+    // vuelvo a hacer un listado de los libros en la parte del lector normal
+    require "vistas/vista_libros.php";
+
+    ?>
+</body>
+
+</html>
