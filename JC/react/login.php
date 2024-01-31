bf0d102edf6f8b46c97fa931017d04d5b6e89fdd
AlvaroMF02 <alvaromarflo@gmail.com>
2024-01-31 12:16:21 +0100

    mm

--- JC/react/login.php
+++ JC/react/login.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *'); 
 
 $_POST = json_decode(file_get_contents("php://input"),true);
- if ($_POST["telefono"]=="alvaro" && $_POST["password"]=="08d6c05a21512a79a1dfeb9d2a8f262f"){
+ if ($_POST["telefono"]=="alvaro" && $_POST["password"]=="81dc9bdb52d04dc20036dbd8313ed055"){
     $respuesta["usuario"]="fulanico";
     $respuesta["mensaje"]="Acceso correcto";
  }else{
