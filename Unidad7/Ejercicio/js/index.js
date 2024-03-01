c3acce283fdf23510800ba329f014a4968ce20cf
alvaro <alvaromarflo@gmail.com>
2024-03-01 11:41:19 +0100

    no va nada bieeeen

--- Unidad7/Ejercicio/js/index.js
+++ Unidad7/Ejercicio/js/index.js
@@ -1,4 +1,4 @@
-const DIR_SERV = "http://localhost/Proyectos/Unidad6/Actividades/Activ2/servicios_rest"
+const DIR_SERV = "http://localhost/Proyectos/Unidad7/Ejercicio/servicios_rest";
 
 $(function(){
     obtener_prod()
---
+++ Unidad7/Ejercicio/servicios_rest/src/func_const.php
@@ -0,0 +1,233 @@
+<?php
+define("SERVIDOR_BD", "localhost:3307");
+// define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "jose");
+define("CLAVE_BD", "josefa");
+define("NOMBRE_BD", "bd_tienda");
+
+// devuelve array con productos
+function obtener_productos()
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+        // Esas dos se pueden hacer en una línea: return array("mensaje_error"=>"No se ha podido conectar a la base de datos: ".e->getMessage());
+    }
+
+    try {
+        $consulta = "select * from producto";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute();
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["productos"] = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// devuelve un producto con el mismo codigo
+function obtener_producto($codigo)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    try {
+        $consulta = "select * from producto where cod=?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$codigo]);
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    if ($sentencia->rowCount() > 0) {
+        $respuesta["producto"] = $sentencia->fetch(PDO::FETCH_ASSOC);
+    } else {
+        $respuesta["mensaje"] = "El producto con código $codigo no se encuentra en la BD";
+    }
+
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// inserta un producto
+function insertar_producto($datos)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    try {
+        $consulta = "insert into producto(cod, nombre, nombre_corto, descripcion, PVP, familia) values (?,?,?,?,?,?)";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute($datos);
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["mensaje"] = "El producto se ha insertado correctamente";
+
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// actualiza un producto
+function actualizar_producto($datos)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    try {
+        $consulta = "update producto set nombre=?, nombre_corto=?, descripcion=?, PVP=?, familia=? where cod=?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute($datos);
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["mensaje"] = "El producto se ha actualizado correctamente";
+
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// Borra un producto por le codigo
+function borrar_producto($codigo)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    try {
+        $consulta = "delete from producto where cod=?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$codigo]); // Al no ser un array, tenemos que pasarselo entre llaves
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["mensaje"] = "El producto se ha borrado correctamente";
+
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// Obtiene las familias
+function obtener_familias()
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+        // Esas dos se pueden hacer en una línea: return array("mensaje_error"=>"No se ha podido conectar a la base de datos: ".e->getMessage());
+    }
+
+    try {
+        $consulta = "select * from familia";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute();
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["productos"] = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// Comprueba repes al insertar
+function repetido($tabla, $columna, $valor)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+        // Esas dos se pueden hacer en una línea: return array("mensaje_error"=>"No se ha podido conectar a la base de datos: ".e->getMessage());
+    }
+
+    try {
+        $consulta = "select * from $tabla where $columna = ?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$valor]);
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["repetido"] = ($sentencia->rowCount()) > 0; // Nos devuelve true si está repetido y false si no
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
+
+// Comprueba repes al editar
+function repetido_editar($tabla, $columna, $valor, $columna_id, $valor_id)
+{
+    try {
+        $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    } catch (PDOException $e) {
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    try {
+        $consulta = "select * from $tabla where $columna = ? AND $columna_id <> ?";
+        $sentencia = $conexion->prepare($consulta);
+        $sentencia->execute([$valor, $valor_id]);
+    } catch (PDOException $e) {
+        $sentencia = null;
+        $conexion = null;
+        $respuesta["mensaje_error"] = "No se ha podido conectar a la base de datos: " . $e->getMessage();
+        return $respuesta;
+    }
+
+    $respuesta["repetido"] = ($sentencia->rowCount()) > 0; // true si esta repe
+    $sentencia = null;
+    $conexion = null;
+    return $respuesta;
+}
