37659fc3db3ac32f641d10ecbeb321a1cd57f872
AlvaroMF02 <alvaromarflo@gmail.com>
2024-03-04 09:10:31 +0100

    borrar funcional

--- Unidad7/Ejercicio/js/index.js
+++ Unidad7/Ejercicio/js/index.js
@@ -1,6 +1,6 @@
 const DIR_SERV = "http://localhost/Proyectos/Unidad7/Ejercicio/servicios_rest";
 
-$(function(){
+$(function () {
     obtener_prod()
 })
 
@@ -16,15 +16,15 @@ function obtener_prod () {
             } else {
 
                 let tablaProd = "<table>"
-                tablaProd += "<tr><th>COD</th><th>Nombre corto</th><th>PVP</th><th>Producto+</th></tr>"
+                tablaProd += "<tr><th>COD</th><th>Nombre corto</th><th>PVP</th><th><button class='enlace' onClick=''>Producto+</button></th></tr>"
 
                 $.each(data.productos, function (key, tupla) {
                     tablaProd += "<tr>"
-                    tablaProd += "<td><button class='enlace' onClick='verDetalle(\""+tupla['cod']+"\")'>" + tupla["cod"] + "</button></td>"
+                    tablaProd += "<td><button class='enlace' onClick='verDetalle(\"" + tupla['cod'] + "\")'>" + tupla["cod"] + "</button></td>"
                     tablaProd += "<td>" + tupla["nombre_corto"] + "</td>"
                     tablaProd += "<td>" + tupla["PVP"] + "</td>"
-                    tablaProd += "<td><button class='enlace' >Borrar</button> - "
-                    tablaProd += "<button class='enlace' >Editar</button></td>"
+                    tablaProd += "<td><button class='enlace' onClick='dudaBorr(\"" + tupla['cod'] + "\")'>Borrar</button> - "
+                    tablaProd += "<button class='enlace' onClick=''>Editar</button></td>"
                     tablaProd += "</tr>"
                 })
 
@@ -49,14 +49,15 @@ function verDetalle (codProd) {
         .done(function (data) {
             if (data.mensaje_error) {
                 $("#detalles").html(data.mensaje_error)
-            } else if(data.mensaje){
+            } else if (data.mensaje) {
                 $("#detalles").html(data.mensaje_error)
-            }else{
+            } else {
                 detalle = "<p><strong>Codigo:</strong>" + codProd + "<br>"
                 detalle += "<strong>Nombre corto:</strong>" + data["producto"]["nombre_corto"] + "<br>"
                 detalle += "<strong>Descripción:</strong>" + data["producto"]["descripcion"] + "<br>"
                 detalle += "<strong>PVP:</strong>" + data["producto"]["PVP"] + "<br>"
                 detalle += "<strong>Familia:</strong>" + data["producto"]["familia"] + "</p>"
+                detalle += "<button onClick='vaciar()'>Volver</button>"
             }
 
             $("#detalles").html(detalle)
@@ -68,6 +69,38 @@ function verDetalle (codProd) {
         })
 
 }
+
+function dudaBorr (cod) {
+
+    let texto = "¿Está seguro de que desea borrar el producto " + cod +"? <br>"
+    texto += "<button onClick='vaciar()'>Volver</button> "
+    texto += "<button onClick='borrar(\"" + cod + "\")'>Borrar</button>"
+
+    $("#detalles").html(texto)
+}
+
+function vaciar () {
+    $("#detalles").html("")
+}
+function borrar (cod) {
+    $.ajax({
+        url: DIR_SERV + "/producto/borrar/" + cod,
+        dataType: "json",
+        type: "DELETE"
+    })
+        .done(function (data) {
+            $("#detalles").html("Producto borrado")
+            location.reload()
+        })
+
+        .fail(function (a, b) {
+            $("#errores").html(error_ajax_jquery(a, b))
+        })
+}
+function vaciar () {
+    $("#detalles").html("")
+}
+
 
 function error_ajax_jquery (jqXHR, textStatus) {
     var respuesta;
