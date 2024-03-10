5387f88ff1d5da04c42b0ee5ca9abab5dba58962
alvaro <alvaromarflo@gmail.com>
2024-03-10 23:37:46 +0100

    ns

--- Unidad7/Ejercicio/js/index.js
+++ Unidad7/Ejercicio/js/index.js
@@ -34,15 +34,12 @@ function mostrar_tabla () {
             }
 
         })
-
         .fail(function (a, b) {
             $("#tabla").html(error_ajax_jquery(a, b))
         })
-
 }
 
 function formInsertar () {
-
     $.ajax({
         url: DIR_SERV + "/familias",
         dataType: "json",
@@ -68,9 +65,6 @@ function formInsertar () {
             $("#detalles").html(error_ajax_jquery(a, b))
             mostrar_tabla()
         })
-
-
-
 }
 
 // VER DETALLES DE X PRODUCTO
@@ -137,6 +131,129 @@ function borrar (cod) {
 function vaciar () {
     $("#detalles").html("")
 }
+
+// -------------------------------------------------------------------------------------
+
+async function formInsertar () {
+    vaciar()
+    let formulario = `<form onsubmit='crearProducto(event)' method='post' action='.'>
+                      <h2>Añadir un producto</h2>
+                      <p>
+                        <label for='cod'><strong>Código: </strong></label><input type='text' name='cod' id='cod' required />
+                        <span class='error'></span>
+                      </p>
+                      <p>
+                        <label for='nombre'><strong>Nombre: </strong></label><input type='text' name='nombre' id='nombre' />
+                        <span class='error'></span>
+                      </p>
+                      <p>
+                        <label for='nombre_cor'><strong>Nombre corto: </strong></label><input type='text' name='nombre_cor' id='nombre_cor' required/>
+                        <span class='error'></span>
+                      </p>
+                      <p>
+                        <label for='desc'><strong>Descripción: </strong></label><textarea name='desc' id='desc' required ></textarea>
+                        <span class='error'></span>
+                      </p>
+                      <p>
+                        <label for='pvp'><strong>PVP: </strong></label><input type='number' name='pvp' id='pvp' required />
+                        <span class='error'></span>
+                      </p>
+    `
+    // añado las familias del select
+    const respuesta = await $.ajax({
+      url: DIR_SERV + "/familias",
+      dataType: "json",
+      type: "GET",
+    })
+  
+    // Si devuelve error lo pongo
+    if (respuesta.error) {
+      $("#detalles").html(respuesta.error)
+    } else if (respuesta.mensaje_error) {
+      $("#detalles").html(respuesta.mensaje_error)
+    } else {
+      // Si no hay error se ponen las familias
+      formulario += `<p>
+                    <label for='familia'><strong>Familia: </strong></label>
+                    <select name='familia' id='familia'>`
+      $.each(respuesta.familias, function (key, tupla) {
+        formulario += `<option value='${tupla.cod}' >${tupla.nombre}</option>`
+      })
+      formulario += `</select></p>
+                    <p><button type='submit'>Añadir</button> <button onClick='vaciar()'>Volver</button></p>
+                    </form>`
+      $("#detalles").html(formulario)
+    }
+  }
+  
+  async function comprobarRepetido (tabla, columna, valor) {
+    const respuesta = $.ajax({
+      url: encodeURI(`${DIR_SERV}/repetido/${tabla}/${columna}/${valor}`),
+      dataType: 'json',
+      type: 'get'
+    })
+  
+    if (respuesta.error) return true
+  
+    if (respuesta.mensaje_error) return true
+  
+    return respuesta.repetido
+  }
+  
+  async function crearProducto (event) {
+    event.preventDefault()
+    const form = event.target
+  
+    let codRepetido = await comprobarRepetido('producto', 'cod', form.cod.value)
+    let nombreCorRepetido = await comprobarRepetido('producto', 'nombre_corto', form.nombre_cor.value)
+    if (codRepetido) {
+      $(form.cod).siblings("span.error").html('* Código repetido *')
+    }
+    if (nombreCorRepetido) {
+      $(form.nombre_cor).siblings("span.error").html('* Código repetido *')
+    }
+  }
+  
+  // Vamos a hacer la tabla con los productos
+  async function obtener_detalles (cod) {
+    $.ajax({
+      url: DIR_SERV + "/productos/" + cod,
+      dataType: "json",
+      type: "GET",
+    }).done(async function (data) {
+      if (data.mensaje_error) {
+        $("#detalles").html(data.mensaje_error)
+      } else {
+        // Vamos a crear una tabla con los productos
+        let detalles = `<h2>Detalles del producto: ${data.producto.cod}</h2>`
+        detalles += `<p><strong>COD:</strong> ${data.producto.cod}</p>`
+        detalles += `<p><strong>Nombre:</strong> ${data.producto.nombre ?? ''}</p>`
+        detalles += `<p><strong>Nombre corto:</strong> ${data.producto.nombre_corto}</p>`
+        detalles += `<p><strong>Descripción:</strong> ${data.producto.descripcion}</p>`
+        detalles += `<p><strong>PVP:</strong> ${data.producto.PVP}</p>`
+  
+        // Pido el nombre de la familia a la API
+        const respuesta = await $.ajax({
+          url: DIR_SERV + "/familia/" + data.producto.familia,
+          dataType: "json",
+          type: "GET",
+        })
+  
+        if (respuesta.error) return $("#detalles").html(respuesta.error)
+  
+        if (respuesta.mensaje_error) return $("#detalles").html(respuesta.mensaje_error)
+  
+        detalles += `<p><strong>Familia: </strong> ${respuesta.familia.nombre}</p>`
+  
+        detalles += `<p><button onclick='vaciar()' >volver</button></p>`
+  
+  
+        $("#acciones").html(detalles)
+      }
+    })
+  }
+  
+// -------------------------------------------------------------------------------------
 
 // ERROR DE AJAX
 function error_ajax_jquery (jqXHR, textStatus) {
