b02ade974bb675e6edba050ee30465eb9cbf7f8d
AlvaroMF02 <alvaromarflo@gmail.com>
2024-03-11 08:52:28 +0100

    obtener familias no funciona

---
+++ Unidad7/Pract1/servicios_rest/index.php
@@ -0,0 +1,68 @@
+<?php
+
+require "src/func_const.php";
+
+require __DIR__ . '/Slim/autoload.php';
+
+$app = new \Slim\App;
+
+// Información de todos los productos
+$app->get("/productos", function () {
+    echo json_encode(obtener_productos());
+});
+
+// Todos los datos del producto cuyo código pongamos
+$app->get("/producto/{cod}", function ($request) { // A la function le tenemos que pasar una request si tenemos un parámetro en la url
+    $producto = $request->getAttribute("cod");
+    echo json_encode(obtener_producto($producto));
+});
+// Familia específica
+$app->get("/familia/{cod}", function ($request) { // A la function le tenemos que pasar una request si tenemos un parámetro en la url
+    $producto = $request->getAttribute("cod");
+    echo json_encode(obtener_famil_cod($producto));
+});
+
+// Insertaremos los productos que le pasaremos por un formulario
+$app->post("/producto/insertar", function ($request) {
+    $datos[] = $request->getParam("cod");
+    $datos[] = $request->getParam("nombre");
+    $datos[] = $request->getParam("nombre_corto");
+    $datos[] = $request->getParam("descripcion");
+    $datos[] = $request->getParam("PVP");
+    $datos[] = $request->getParam("familia");
+
+    echo json_encode(insertar_producto($datos)); // Los datos los tenemos en el orden que aparecen en el insert
+});
+
+// Actualizaremos todos los datos del producto especificado
+$app->put("/producto/actualizar/{cod}", function ($request) {
+    $datos[] = $request->getParam("nombre");
+    $datos[] = $request->getParam("nombre_corto");
+    $datos[] = $request->getParam("descripcion");
+    $datos[] = $request->getParam("PVP");
+    $datos[] = $request->getParam("familia");
+    $datos[] = $request->getAttribute("cod");
+
+    echo json_encode(actualizar_producto($datos));
+});
+
+// Borrar un producto con un código
+$app->delete("/producto/borrar/{cod}", function ($request) {
+    echo json_encode(borrar_producto($request->getAttribute("cod")));
+});
+
+// Información de todas las familias
+$app->get("/familias", function () {
+    echo json_encode(obtener_familias());
+});
+
+// Nos devuelve verdadero si ya existe el valor en una columna de una tabla al insertar
+$app->get("/repetido/{tabla}/{columna}/{valor}", function ($request) {
+    echo json_encode(repetido($request->getAttribute("tabla"), $request->getAttribute("columna"), $request->getAttribute("valor")));
+});
+
+// Nos devuelve verdadero si ya existe el valor en una columna de una tabla al editar
+$app->get("/repetido/{tabla}/{columna}/{valor}/{columna_id}/{valor_id}", function ($request) {
+    echo json_encode(repetido_editar($request->getAttribute("tabla"), $request->getAttribute("columna"), $request->getAttribute("valor"), $request->getAttribute("columna_id"), $request->getAttribute("valor_id")));
+});
+$app->run();
